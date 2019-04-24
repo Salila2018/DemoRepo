2900c244d4ec0f767166fd659520c2fd74c4204e
Lily Behera <lilybehera@yahoo.com>
2019-04-24 14:26:48 -0500

    First Commit.........

--- src/main/java/com/qa/libs/TestBase.java
+++ src/main/java/com/qa/libs/TestBase.java
@@ -1,5 +1,8 @@
 package com.qa.libs;
-
+//https://www.youtube.com/watch?v=HCeBd5GKNO8&t=1146s
 public class TestBase {
+	public void test() {
+		System.out.println("Test Method");
 
+}
 }
--- src/main/java/com/qa/pages/HomePage.java
+++ src/main/java/com/qa/pages/HomePage.java
@@ -1,5 +1,8 @@
 package com.qa.pages;
 
 public class HomePage {
+	public void Homepage() {
+		System.out.println("Home page");
 
+}
 }
